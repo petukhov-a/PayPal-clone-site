a5dc6841203ddc372e577d4f6765dec8fcb49c99
Anton <reopik@mail.ru>
2023-06-03 23:26:52 +0300

    finished navbar for width 1000px

--- script.js
+++ script.js
@@ -7,11 +7,23 @@ if (window.innerWidth < 1000) {
     
     menuIcon.addEventListener('click', () => {
         navbar.classList.toggle('change');
+
+        if (!navbar.classList.contains('change')) {
+            document.querySelectorAll('.nav-dropdown').forEach(dropdown => {
+                dropdown.style.left = '-20rem'
+            });
+        }
     });
 
     document.querySelectorAll('.show-dropdown').forEach(link => {
         link.addEventListener('click', () => {
             link.nextElementSibling.style.left = '0'
+        });
+    });
+
+    document.querySelectorAll('.dropdown-heading-link').forEach(headingLink => {
+        headingLink.addEventListener('click', () => {
+            headingLink.parentElement.parentElement.style.left = '-20rem';
         });
     });
 } else {
